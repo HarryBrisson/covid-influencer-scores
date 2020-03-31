52aeedd729d5de0f76d748405c46be3e856dffa5
HarryBrisson <ejbrisson@gmail.com>
2020-03-30 23:24:02 -0700

    add function to pull growth trajectory

--- calc_key_dates.py
+++ calc_key_dates.py
@@ -17,6 +17,21 @@ def get_estimated_date(currentdeathcount,event,growthrate):
 	date_estimate = get_date_from_today(offset=number_of_days-1,fmt='%A %d %B %Y')
 	return date_estimate
 
+def get_growth_trajectory(currentdeathcount,event,growthrate):
+	death_counts = get_death_counts()
+	comparison_death_count = death_counts[event]['deaths']
+	number_of_days = math.log(float(comparison_death_count)/float(currentdeathcount),growthrate)
+	trajectory = []
+	deathcount = currentdeathcount
+	for d in range(int(number_of_days)+1):
+		deathcount = deathcount*growthrate
+		row = {
+			'date': get_date_from_today(offset=d,fmt='%A %d %B %Y'),
+			'deaths': deathcount
+			}
+		trajectory.append(row)
+	return trajectory
+
 def get_comparisons():
 	data = get_death_counts()
 	return data.keys()
